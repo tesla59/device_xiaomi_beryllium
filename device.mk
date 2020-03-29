9d7c18bffec08178659bc9ad512938298ce54383
tesla59 <talktonishantsingh.ns@gmail.com>
2020-03-29 08:19:58 +0000

    beryllium: drop NoCutOverlay cuz its already defined by source

--- device.mk
+++ device.mk
@@ -23,9 +23,6 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
-PRODUCT_PACKAGES += \
-    NoCutoutOverlay
-
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
